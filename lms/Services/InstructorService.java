ca541df3c868e364883b0e62480923404ba56aa9
Mohammed Alhajri <61750916+mmyh147@users.noreply.github.com>
2024-03-17 21:24:23 +0300

    Update InstructorService.java
    add more services

--- lms/Services/InstructorService.java
+++ lms/Services/InstructorService.java
@@ -1,16 +1,20 @@
 package com.example.lms.Services;
 
+import com.example.lms.Model.Course;
 import com.example.lms.Model.Instructor;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@AllArgsConstructor
 @Service
 public class InstructorService {
 
 
 
     ArrayList<Instructor> instructorList = new ArrayList<>();
+    private final CourseService courseService;
 
     public ArrayList<Instructor> getAllUsers() {
         return instructorList;
@@ -57,4 +61,49 @@ public class InstructorService {
         }
         return null;
     }
+
+//return the courses for the instructor
+    public ArrayList<Course> getAllTheCourses(String id){
+        ArrayList<Course> allCourses = new ArrayList<>();
+        for (Course course : courseService.courseList){
+            if (course.getInstructor().getId().equals(id)){
+                allCourses.add(course);
+            }
+        }
+        return allCourses;
+    }
+
+
+    //return number of courses for the instructor
+    public int getNumberOfCourses(String id){
+        int sum =0;
+        for (Course course : courseService.courseList){
+            if (course.getInstructor().getId().equals(id)){
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+
+    //change instructor
+    public boolean changeInstructor(String courseId, String instructorID){
+
+
+
+        for (Course course : courseService.courseList){
+            if (course.getId().equals(courseId)) {
+
+
+                for (Instructor instructor : instructorList) {
+
+                    if (instructor.getId().equals(instructorID)) {
+                        courseService.courseList.get(courseService.courseList.indexOf(courseId)).setInstructor(instructorList.get(instructorList.indexOf(instructorID)));
+                        return true;
+                    }
+                }
+            }}
+        return false;
+    }
+
 }
